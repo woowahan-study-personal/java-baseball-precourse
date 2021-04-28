6c9e823a777d0edeb47ead35bacf3aa2398ddcd4
Song Eun Seok <eunsuk93@naver.com>
2021-04-29 00:09:09 +0900

    create getRandomNumbers method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,31 @@
 package baseball;
 
 import java.util.Scanner;
+import utils.RandomUtils;
 
 public class Application {
+    final static int maxNumberLength = 3;
+    
+    public static int[] getRandomNumbers() {
+        int[] numbers = new int[3];
+        int[] visitNumbers = new int[10];
+        for (int i = 0; i < maxNumberLength; i++) {
+            while (true) {
+                int number = RandomUtils.nextInt(0, 9);
+                if (i == 0 & number == 0) {
+                    continue;
+                }
+                if (visitNumbers[number] == 1) {
+                    continue;
+                }
+                numbers[i] = number;
+                visitNumbers[number] = 1;
+                break;
+            }
+        }
+        return numbers;
+    }
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
