7e1a62f10012ea560b8ae2f5f63bb184bd13339e
송다혜 <dada3254@daum.net>
2021-05-03 02:37:20 +0900

    fix : 문구 한글 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        System.out.println("Let's play game~~~");
+        System.out.println("Let's play game");
 
         boolean startGame = restartGame(scanner);
         int strike = 0;
         int ball = 0;
-        
+
         // 1 게임 반복
         while (startGame) {
             // 랜덤수발생 randomNum
@@ -19,7 +19,7 @@ public class Application {
             // 2 입력비교 반복
             while (strike != 3) {
                 // 내숫자입력 userNum
-                System.out.println("userNum : ");
+                System.out.print("숫자를 입력해주세요 : ");
                 String userNum = scanner.nextLine();
                 int[] compareResult = compareNum(randomNum, userNum);
                 // 랜덤수 = 내숫자비교 [f]
@@ -54,7 +54,7 @@ public class Application {
 
     // 게임 재시작 [f]
     public static boolean restartGame(Scanner scanner) {
-        System.out.println("restart 1, end 2");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         int restartGameNum = Integer.parseInt(scanner.nextLine());
         if (restartGameNum != 1 && restartGameNum != 2) {
             throw new IllegalArgumentException("error");
@@ -68,18 +68,18 @@ public class Application {
     public static void resultGame(int ball, int strike) {
         String result = "";
         if (strike == 3) {
-            result = "3 strike! end";
+            result = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
         } else if (ball == 0 && strike == 0) {
-            result = "nothing";
+            result = "낫싱";
         } else {
             if (ball != 0 && strike != 0) {
-                result = ball +"ball "+ strike +"strike";
+                result = ball +"볼 "+ strike +"스트라이크";
             }
             else if (ball != 0) {
-                result = ball +"ball";
+                result = ball +"볼";
             }
             else if (strike != 0) {
-                result = strike +"strike";
+                result = strike +"스트라이크";
             }
         }
         System.out.println(result);
