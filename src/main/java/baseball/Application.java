64634df66b57bcc0f40ecd4c1e22850d509659ca
송다혜 <dada3254@daum.net>
2021-05-03 00:17:48 +0900

    feat : Add 게임재시작 함수,  게임결과출력 함수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,63 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO 구현 진행
+        System.out.println("Let's play game~~~");
+        boolean startGame = restartGame(scanner);
+        int strike = 0;
+        int ball = 0;
+        // 2 게임 반복
+        while (startGame) {
+            // 랜덤수발생 randomNum
+
+            // 3 입력 반복
+            while (strike != 3) {
+                // 내숫자입력 userNum
+                // 랜덤수 = 내숫자비교 [f]
+
+                // strike = 값;
+                // ball = 값;
+                // 볼/스 결과출력 [f]
+                resultGame(ball, strike);
+                strike = 3;
+            }
+            strike = 0;
+            ball = 0;
+            // 게임 재시작 [f]
+            startGame = restartGame(scanner);
+        }
     }
+
+    // 게임 재시작 [f]
+    public static boolean restartGame(Scanner scanner) {
+        System.out.println("restart 1, end 2");
+        int restartGameNum = scanner.nextInt();
+        if (restartGameNum != 1 && restartGameNum != 2) {
+            throw new IllegalArgumentException("error");
+        } else if (restartGameNum == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    // 볼/스 결과출력 [f]
+    public static void resultGame(int ball, int strike) {
+        String result = "";
+        if (strike == 3) {
+            result = "3 strike! end";
+        } else if (ball == 0 && strike == 0) {
+            result = "nothing";
+        } else {
+            if (ball != 0 && strike != 0) {
+                result = ball +"ball "+ strike +"strike";
+            }
+            else if (ball != 0) {
+                result = ball +"ball";
+            }
+            else if (strike != 0) {
+                result = strike +"strike";
+            }
+        }
+        System.out.println(result);
+    }
+
 }
