e03288a450c977bd75e4070a9702f74255fc344e
dreamfinder2019 <dreamfinder2019@gmail.com>
2021-05-03 20:23:56 +0900

    feat(elements): NumberTools

---
+++ src/main/java/baseball/elements/NumberTools.java
@@ -0,0 +1,36 @@
+package baseball.elements;
+import utils.RandomUtils;
+
+public class NumberTools {
+    private static final int minimumGameNumber = 111;
+    private static final int maximumGameNumber = 999;
+    private static final int digitLimit = 1;
+
+    private NumberTools() {}
+
+    public static boolean verify(int number) {
+        String numberString = Integer.toString(number);
+
+        if (minimumGameNumber > number || number > maximumGameNumber || numberString.contains("0")) {
+            return false;
+        }
+
+        for (char digit : numberString.toCharArray()) {
+            if (numberString.chars().filter(currentDigit -> currentDigit == digit).count() > digitLimit) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int generateRandomNumber() {
+        int targetGameNumber = RandomUtils.nextInt(minimumGameNumber, maximumGameNumber);
+
+        while (!verify(targetGameNumber)) {
+            targetGameNumber = RandomUtils.nextInt(minimumGameNumber, maximumGameNumber);
+        }
+
+        return targetGameNumber;
+    }
+}
