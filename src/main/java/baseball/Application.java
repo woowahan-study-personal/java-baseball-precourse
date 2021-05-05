06e5d2961cf1f3ec09767bb7faaa7b945598db2b
cubodado <dalkang000@gmail.com>
2021-05-05 21:03:03 +0900

    fix: 3스트라이크일 경우 ball, strike 변수 초기화 > 낫싱 출력 버그 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -58,11 +58,11 @@ public class Application {
             strike = 0;
             System.out.println(message);
             correctNumbers = true;
-        } if (ball != 0) {
+        } else if (ball != 0) {
             result.append(ball + "볼 ");
-        } if (strike != 0) {
+        } else if (strike != 0) {
             result.append(strike + "스트라이크");
-        } if (ball == 0 && strike ==0) {
+        } else if (ball == 0 && strike ==0) {
             result.append("낫싱");
         }
 
