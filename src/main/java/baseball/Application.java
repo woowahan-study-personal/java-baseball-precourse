258a297a9ec5126a04db56c4bc59fc7b243c0903
장주환 <juhwan8956@naver.com>
2021-05-06 15:36:17 +0900

    [숫자야구게임] 장주환 1차 제출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,79 +4,86 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.Iterator;
+
 import utils.RandomUtils;
 
 public class Application {
+    private static int lenOfCount = 3;
+    private static Boolean isReplay = true;
+    private static Boolean isOver = true;
+    private static int cntBall = 0;
+    private static int cntStrike = 0;
+
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO 구현 진행
-        Boolean isReplay = true;
 
         while (isReplay) {
             int[] userNum = new int[3];
             int[] randNum = new int[3];
             Set<Integer> randSet = new HashSet<Integer>();
-            Boolean isOver = true;
-
+            isOver = true;
             setRandomNum(randSet, randNum);
 
             while (isOver) {
-                int cntBall = 0;
-                int cntStrike = 0;
-                int lenOfCount = 3;
+                cntBall = 0;
+                cntStrike = 0;
 
                 System.out.println("Input numbers : ");
                 int userInput = scanner.nextInt();
 
                 setUserNum(userNum, userInput);
 
-                for (int i = 0; i < lenOfCount; i++) {
-                    cntStrike = countStrike(userNum, randNum, i, cntStrike);
-                    cntBall = countBall(userNum, randNum, i, cntBall);
-                }
+                cntCounter(userNum, randNum);
 
-                isOver = checkCounter(cntBall, cntStrike, isOver);
+                checkCounter(cntBall, cntStrike);
+
+                if (cntStrike == lenOfCount) {
+                    isOver = false;
+                }
             }
-            if (!isOver) {
-                System.out.println("Enter 1 to start a new game, 2 to end the game.");
-                int tmp = scanner.nextInt();
-                isReplay = askReplay(tmp);
-            }
+        
+            System.out.println("Enter 1 to start a new game, 2 to end the game.");
+            int tmp = scanner.nextInt();
+            askReplay(tmp);
         }
         scanner.close();
     }
 
-    public static Boolean askReplay(int tmp) {
-        boolean check = false;
-        if(tmp == 1){
-            check = true;
-        }else if(tmp == 2){
-            check = false;
-        }else{
-            throw new IllegalArgumentException("Enter 1 or 2");
+    private static void cntCounter(int[] userNum, int[] randNum) {
+        for (int i = 0; i < lenOfCount; i++) {
+            cntStrike = countStrike(userNum, randNum, i, cntStrike);
+            cntBall = countBall(userNum, randNum, i, cntBall);
         }
-        return check;
     }
 
-    public static boolean checkCounter(int cntBall, int cntStrike, boolean isOver) {
-        if(cntBall == 0 && cntStrike == 0){
+    public static void askReplay(int tmp) {
+        int replayBT = 1;
+        int stopBT = 2;
+        if (tmp == replayBT) {
+            isReplay = true;
+        } else if (tmp == stopBT) {
+            isReplay = false;
+        } else {
+            throw new IllegalArgumentException("Enter 1 or 2");
+        }
+    }
+
+    public static void checkCounter(int cntBall, int cntStrike) {
+        if (cntBall == 0 && cntStrike == 0) {
             System.out.println("Nothing");
-        }else if(cntStrike == 3){
+        } else if (cntBall == 0) {
             System.out.println(cntStrike + "Strike");
-            isOver = false;
-        }else if(cntBall == 0){
-            System.out.println(cntStrike + "Strike");
-        }else if(cntStrike == 0){
+        } else if (cntStrike == 0) {
             System.out.println(cntBall + "Ball");
-        }else{
+        } else {
             System.out.println(cntBall + "Ball" + cntStrike + "Strike");
         }
-
-        return isOver;
     }
 
     public static void setUserNum(int[] userNum, int userInput) {
-        for (int i = 0; i < 3; i++) {
+
+        for (int i = 0; i < lenOfCount; i++) {
             userNum[i] = userInput % 10;
             userInput /= 10;
         }
@@ -85,28 +92,26 @@ public class Application {
     public static void setRandomNum(Set<Integer> randSet, int[] randNum) {
         do {
             randSet.add(RandomUtils.nextInt(1, 9));
-        } while (randSet.size() < 3);
-        Iterator iter = randSet.iterator();
+        } while (randSet.size() < lenOfCount);
+        Iterator<Integer> iter = randSet.iterator();
         for (int i = 0; i < 3; i++) {
             randNum[i] = (int) iter.next();
         }
     }
 
-
-    public static int countStrike(int[] user, int[] com, int i, int st){
+    public static int countStrike(int[] user, int[] com, int i, int st) {
         if (user[i] == com[i]) {
             st++;
         }
         return st;
     }
-    public static int countBall(int[] user, int[] com, int i, int bl){
-        for(int j=0; j<3; j++) {
-            if(i != j && user[i] == com[j]){
+
+    public static int countBall(int[] user, int[] com, int i, int bl) {
+        for (int j = 0; j < lenOfCount; j++) {
+            if (i != j && user[i] == com[j]) {
                 bl++;
             }
         }
         return bl;
     }
-
-
 }
