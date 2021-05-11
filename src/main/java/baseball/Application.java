51fb017b01a4a8af1469a444a74e4120b305c90a
Jihyeok11 <woah68@gmail.com>
2021-05-12 03:16:03 +0900

    기본 동작 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import com.sun.tools.jdeprscan.scan.Scan;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -10,9 +12,26 @@ import utils.RandomUtils;
 public class Application {
 
     public static void main(String[] args) {
-        Referee referee = new Referee();
-        String result = referee.compare(Arrays.asList(3,1,2), Arrays.asList(1,2,3));
-        System.out.println(result);
+        NumberGenerator generator = new NumberGenerator();
+        List<Integer> computer = generator.createRandomNumbers();
 
+        Referee referee = new Referee();
+        String result = "";
+        while (!(result.equals("0볼 3스트라이크"))){
+            result = referee.compare(computer, askNumbers());
+            System.out.println(result);
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+    }
+    public static List<Integer> askNumbers(){
+        System.out.println("숫자를 입력해 주세요 : ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : input.split("")){
+            numbers.add(Integer.valueOf(number));
+        }
+        return numbers;
     }
 }
