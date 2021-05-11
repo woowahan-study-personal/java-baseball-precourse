53abc222960f40851698afec6f1a4005bcd3c98e
Jihyeok11 <woah68@gmail.com>
2021-05-12 02:50:55 +0900

    자릿수 비교

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import utils.RandomUtils;
+// ctrl + alt + L : 자동 포맷터(들여쓰기와 띄어쓰기 자동으로)
 
 public class Application {
 
@@ -14,10 +15,14 @@ public class Application {
 //        List<Integer> numbers = generator.createRandomNumbers();
 //        System.out.println(numbers);
         Judgement judgement = new Judgement();
-        int count = judgement.correctCount(
-            Arrays.asList(1, 2, 4), Arrays.asList(4, 1, 2)
-        );
-        // ctrl + alt + L : 자동 포맷터(들여쓰기와 띄어쓰기 자동으로)
-        System.out.println(count);
+//        int count = judgement.correctCount(
+//            Arrays.asList(1, 2, 4), Arrays.asList(4, 1, 2)
+//        );
+//        System.out.println(count);
+
+
+        boolean place = judgement.hasPlace(Arrays.asList(7, 8, 9), 0, 7);
+        System.out.println(place);
+
     }
 }
