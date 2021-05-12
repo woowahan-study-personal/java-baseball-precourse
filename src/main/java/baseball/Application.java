5873cc6a9d0bf17601da8a14a81842d015aa9abf
Jihyeok11 <woah68@gmail.com>
2021-05-12 21:47:36 +0900

    강의끝낫으니 새로

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,5 @@
 package baseball;
 
-import com.sun.tools.jdeprscan.scan.Scan;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 import utils.RandomUtils;
 // ctrl + alt + L : 자동 포맷터(들여쓰기와 띄어쓰기 자동으로)
@@ -12,26 +7,8 @@ import utils.RandomUtils;
 public class Application {
 
     public static void main(String[] args) {
-        NumberGenerator generator = new NumberGenerator();
-        List<Integer> computer = generator.createRandomNumbers();
-
-        Referee referee = new Referee();
-        String result = "";
-        while (!(result.equals("0볼 3스트라이크"))){
-            result = referee.compare(computer, askNumbers());
-            System.out.println(result);
-        }
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
-    }
-    public static List<Integer> askNumbers(){
-        System.out.println("숫자를 입력해 주세요 : ");
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        List<Integer> numbers = new ArrayList<>();
-        for (String number : input.split("")){
-            numbers.add(Integer.valueOf(number));
-        }
-        return numbers;
+        final Scanner scanner = new Scanner(System.in);
+        int computer = RandomUtils.nextInt(100,999);
+        System.out.println(computer);
     }
 }
